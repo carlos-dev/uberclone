ef4ae9ea14c3f3b75ad03f9c271d596bbf97f6fa
Carlos André <31482507+carlos-dev@users.noreply.github.com>
2020-01-09 01:09:24 -0300

    mapa configurado

--- android/app/src/main/java/com/desafiofrontend/MainApplication.java
+++ android/app/src/main/java/com/desafiofrontend/MainApplication.java
@@ -4,12 +4,10 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
@@ -29,10 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new MapsPackage()
-          );
+          return packages;
         }
 
         @Override
--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
 
-const App = () => (
-  <View>
-    <Text>oi</Text>
-  </View>
-);
+import MapView from 'react-native-maps';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFill,
+    backgroundColor: '#474363',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+})
+
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [coodinates, setCoordinates] = useState({
+    latitude: -23.030815,
+    longitude: -43.472856
+  });
+  return (
+    <View style={styles.container}>
+      { loading ? (
+        <ActivityIndicator size={"large"} />
+      ): (
+        <MapView 
+          initialRegion={{
+            latitude: coodinates.latitude,
+            longitude: coodinates.longitude,
+            latitudeDelta: 0.0068,
+            longitudeDelta: 0.0068
+          }}
+          style={styles.map}
+        >
+
+        </MapView>
+      )}
+   </View>
+  )
+}
 
 export default App;
